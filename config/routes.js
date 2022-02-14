07f7b8a46964ed459cea6948d6b5e7aba7e87afe
Jackson Magnabosco <jackson.magnabosco@globalsys.com.br>
2022-02-14 13:30:51 -0300

    att

--- config/routes.js
+++ config/routes.js
@@ -2,9 +2,18 @@ const express = require("express");
 const routes = express();
 routes.use(express.json());
 const auth = require('../middlewares/auth');
+const path = require('path')
+
+routes.set("view engine", "ejs");
+routes.set('views', path.join(__dirname, '../views'));
+routes.use(express.static(path.join('C:/Users/jacks/Triider/public')));
 
 const UserController = require("../controllers/UserController");
 const NovaPiadaController = require("../controllers/NovaPiadaController");
+
+routes.get("/", async ({res}) => {
+    res.render("index");
+  });
 
 routes.post("/user/register", UserController.create);
 routes.post("/user/login", auth, UserController.login);
